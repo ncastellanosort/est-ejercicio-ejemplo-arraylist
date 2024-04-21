ca2513b80ec385b2f7acc720fdaa5c5c72ec1009
Nicolas Castellanos <nicolasfelipe00234@gmail.com>
2024-04-20 23:28:45 -0500

    finalizado

---
+++ src/logica/Main.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package logica;
+
+import ui.Pantalla;
+
+/**
+ *
+ * @author Nicolas
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Pantalla pantalla = new Pantalla();
+
+        pantalla.setLocationRelativeTo(null);
+        pantalla.setResizable(false);
+        pantalla.setVisible(true);
+
+    }
+
+}
